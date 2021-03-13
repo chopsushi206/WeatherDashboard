0569100ae78ea377dcb8a90c1959d3c3fdf78a5e
LiHua Anderson <lihua.anderson@gmail.com>
2021-03-12 19:17:11 -0800

    finished pulling data for curent day and appending in HTML. currently trying to fix a bug getting the icon to dislay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,67 @@
-const currentDayEl = document.getElementsByClassName('currentDay');
-const searchBtnEl = document.getElementsByClassName('searchBtn');
+// DOM Elements
+const currentDayEl = document.getElementById('currentDay');
+const searchHistoryEl = document.getElementById('searchHistory');
+const searchBtnEl = document.getElementById('searchBtn');
+// Moment variable to display current Date.
+const date = moment().format('M/DD/YYYY');
 
+// function to call fetch and handle promises
 function currentDayApi() {
+    //  variable for api url
     let currentDayUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&appid=e730c08a61ff43dadfd7df6e93ba1be2'
 
+    // fetching that url
     fetch(currentDayUrl)
+        // then returning in json format
         .then(function (response) {
             return response.json();
         })
+        // then using returned data
         .then(function (data) {
-            console.log(data)
+            // console.log test of data to see the array of objects
+            console.log(data.weather[0].icon)
+            // variable to create h2 element
+            // elements content from data as well as moment variable created earlier to display date
+            //  appending that element to another element
+            // repeats for each piece of information needed
             let cityName = document.createElement('h2');
-            cityName.innerHtml = data.name;
-            currentDayEl.append(cityName);
+            let currentIcon = document.createElement('img');
+            currentIcon.src = 'http://openweathermap.org/img/w/' + data.weather[0].icon +'.png';
+            cityName.textContent = data.name + ' (' + date + ') ' + currentIcon;
+            currentDayEl.appendChild(cityName);
             let currentTemp = document.createElement('span');
-            currentTemp.textContent = data.main.temp;
+            currentTemp.textContent = 'Temperature: ' + data.main.temp + '°F';
             currentDayEl.appendChild(currentTemp);
             let currentHumidity = document.createElement('span');
-            currentHumidity.textContent = data.main.humidity;
+            currentHumidity.textContent = 'Humidity: ' + data.main.humidity + '%';
             currentDayEl.appendChild(currentHumidity);
             let currentWind = document.createElement('span');
-            currentWind.textContent = data.wind.speed;
+            currentWind.textContent = 'Wind Speed: ' + data.wind.speed + ' MPH';
             currentDayEl.appendChild(currentWind);
+            let searchCity = document.createElement('li');
+            searchCity.textContent = data.name;
+            searchHistoryEl.appendChild(searchCity);
         })
 }
 
+
 currentDayApi();
+
+function fiveDayApi() {
+    //  variable for api url
+    let fiveDayUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Phoenix&units=imperial&appid=e730c08a61ff43dadfd7df6e93ba1be2'
+
+    // fetching that url
+    fetch(fiveDayUrl)
+        // then returning in json format
+        .then(function (response) {
+            return response.json();
+        })
+        // then using returned data
+        .then(function (data) {
+            // console.log test of data to see the array of objects
+            console.log(data)
+        })
+}
+
+fiveDayApi();
